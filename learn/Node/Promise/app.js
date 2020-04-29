d9557a034a01cc71cd9c4cd56cd4da25a3a549bc
sunyu <1404305564@qq.com>
2020-04-29 18:36:28 +0800

    Node:package promise

--- learn/Node/Promise/app.js
+++ learn/Node/Promise/app.js
@@ -1,43 +1,30 @@
+/*
+	封装Promise的示例
+		省略then方法的第二个函数（用于处理错误）
+*/
 var fs = require("fs");
 
-var p1 = new Promise(function(resolve,reject){
-	fs.readFile('./a.text','utf-8',function(error,data){
-		if(error) return reject(error);
-		resolve(data);
+
+function pReadFile(filePath){
+	// 返回一个Promise容器，这个容器内部有一个异步函数
+	return new Promise(function(resolve,reject){
+		fs.readFile(filePath,'utf-8',function(error,data){
+			if(error) return reject(error);
+			resolve(data);
+		});
 	});
-});
+}
 
-var p2 = new Promise(function(resolve,reject){
-	fs.readFile('./b.text','utf-8',function(error,data){
-		if(error) return reject(error);
-		resolve(data);
+pReadFile("./a.text")
+	.then(function(data){
+		console.log(data);
+		// 执行函数pReadFile，并返回pReadFile的返回值，即又一个Promise对象
+		return pReadFile("./b.text");
+	})
+	.then(function(data){
+		console.log(data);
+		return pReadFile("./c.text");
+	}).then(function(data){
+		console.log(data);
 	});
-});
 
-var p3 = new Promise(function(resolve,reject){
-	fs.readFile('./c.text','utf-8',function(error,data){
-		if(error) return reject(error);
-		resolve(data);
-	});
-});
-
-
-p1
-	.then(
-		function(data){
-			console.log(data);
-			return p2;
-		},
-		function(error){}
-	).then(
-		function(data){
-			console.log(data);
-			return p3;
-		},
-		function(error){}
-	).then(
-		function(data){
-			console.log(data);
-		},
-		function(error){}
-	);
